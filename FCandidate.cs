31db8701ba129ba4adaad5701baffda7821a8539
Tran Trung Hieu <tthieu@gmail.com>
2024-02-04 18:32:11 +0700

    first commit with Candidate Form

---
+++ FCandidate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_Form_Project
+{
+    public partial class frmInfoCand : Form
+    {
+        public frmInfoCand()
+        {
+            InitializeComponent();
+        }
+
+        private void frmInfoCand_Load(object sender, EventArgs e)
+        {
+            txtName.Text = "NGUYỄN VĂN A";
+            txtJob.Text = "Nhân viên kinh doanh";
+        }
+    }
+}
